7844043e69230061b140450121a514a3becc2dbf
Max <max@bluechilli.com>
2018-10-17 23:31:41 +1100

    build

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -7,12 +7,13 @@ param([string]$version)
 
 if ([string]::IsNullOrEmpty($version)) {$version = "0.0.1"}
 
-$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+$msbuild = '"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"'
 &$msbuild ..\interface\IMQTTClient.rx\IMQTTClientRx.csproj /t:Build /p:Configuration="Release"
 #&$msbuild ..\main\MQTTClient.rx\MQTTClientRx.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\MQTTClientRx.netstandard20\MQTTClientRx.netstandard20.csproj /t:Build /p:Configuration="Release"
 #&$msbuild ..\main\MQTTClientRx.UWP\MQTTClientRx.UWP.csproj /t:Build /p:Configuration="Release"
 
+Invoke-Expression 
 
 Remove-Item .\NuGet -Force -Recurse
 New-Item -ItemType Directory -Force -Path .\NuGet
