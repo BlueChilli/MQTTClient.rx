b4800a243632880d91ce9d8a2baee3d149e5a8d3
Max <max@bluechilli.com>
2018-10-18 08:43:53 +1100

    build

--- src/build/publishLocal.ps1
+++
@@ -1,9 +0,0 @@
-param([string]$betaver,
-[string]$key,
-[string]$source,
-[string] $version)
-
-
-.\build.ps1 $version
-
-c:\tools\nuget\nuget.exe push -Source $source -ApiKey $key ".\NuGet\MQTTClientRx.$version.symbols.nupkg"
