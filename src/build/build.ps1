f4e4bb711701d2d186693ff2610edb9b46ec8a7b
Max <max@bluechilli.com>
2018-10-17 23:17:58 +1100

    build

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -2,7 +2,14 @@ param([string]$version)
 
 if ([string]::IsNullOrEmpty($version)) {$version = "0.0.1"}
 
-$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+$msbuild = vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath
+if ($path) {
+  $path = join-path $path 'MSBuild\15.0\Bin\MSBuild.exe'
+  if (test-path $path) {
+    & $path $args
+  }
+}
+
 &$msbuild ..\interface\IMQTTClient.rx\IMQTTClientRx.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\MQTTClientRx.netstandard20\MQTTClientRx.netstandard20.csproj /t:Build /p:Configuration="Release"
 
